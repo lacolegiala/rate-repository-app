55d057dc29102e9ad3c9d2748b1c6e0cf403719b
Sofia Riekkola <sofiariekkola@Sofia-MacBook-Pro-2.local>
2022-09-09 17:44:26 +0300

    Modify stuff so that it works

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-// import { Route, Switch, Redirect } from 'react-router-native';
+import { Route, Routes } from 'react-router-native';
 
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
-// import SignIn from './SignIn';
+import SignIn from './SignIn';
 
 const styles = StyleSheet.create({
   container: {
@@ -18,15 +18,10 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      {/* <Switch>
-        <Route path="/" exact>
-          <RepositoryList />
-        </Route>
-        <Route path="/signin">
-          <SignIn />
-        </Route>
-        <Redirect to="/" />
-      </Switch> */}
+      <Routes>
+        <Route path="/" element={<RepositoryList />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
     </View>
   );
 };
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,79 +1,79 @@
-// import React from 'react';
-// import { View, StyleSheet, Pressable } from 'react-native'
-// import FormikTextInput from './FormikTextInput';
-// import { Formik } from 'formik'
-// import * as yup from 'yup';
+import React from 'react';
+import { View, StyleSheet, Pressable } from 'react-native'
+import FormikTextInput from './FormikTextInput';
+import { Formik } from 'formik'
+import * as yup from 'yup';
 
-// import Text from './Text'
-// import useSignIn from '../hooks/useSignIn';
-// import AuthStorage from '../utils/authStorage';
+import Text from './Text'
+import useSignIn from '../hooks/useSignIn';
+import AuthStorage from '../utils/authStorage';
 
-// const validationSchema = yup.object().shape({
-//   username: yup.string().required('Username is required'),
-//   password: yup.string().required('Password is required')
-// })
+const validationSchema = yup.object().shape({
+  username: yup.string().required('Username is required'),
+  password: yup.string().required('Password is required')
+})
 
 
-// const styles = StyleSheet.create({
-//   formCard: {
-//     backgroundColor: 'white'
-//   },
-//   submitButton: {
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     paddingVertical: 12,
-//     paddingHorizontal: 32,
-//     borderRadius: 4,
-//     elevation: 3,
-//     backgroundColor: '#2A64CC',
-//     marginVertical: 12,
-//     marginHorizontal: 10
-//   },
-// })
+const styles = StyleSheet.create({
+  formCard: {
+    backgroundColor: 'white'
+  },
+  submitButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#2A64CC',
+    marginVertical: 12,
+    marginHorizontal: 10
+  },
+})
 
-// type SignInFormValues = {
-//   username: string,
-//   password: string
-// }
+type SignInFormValues = {
+  username: string,
+  password: string
+}
 
-// const SignIn = () => {
-//   const [signIn] = useSignIn()
-//   const initialValues: SignInFormValues = {
-//     username: '',
-//     password: ''
-//   }
+const SignIn = () => {
+  const [signIn] = useSignIn()
+  const initialValues: SignInFormValues = {
+    username: '',
+    password: ''
+  }
 
-//   const onSubmit = async (values: SignInFormValues) => {
-//     const { username, password } = values;
+  const onSubmit = async (values: SignInFormValues) => {
+    const { username, password } = values;
 
-//     try {
-//       const { data } = await signIn({ username: username, password: password });
-//       const storage = new AuthStorage()
-//       if (data) {
-//         await storage.setAccessToken(data.authenticate.accessToken)
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
+    try {
+      const { data } = await signIn({ username: username, password: password });
+      const storage = new AuthStorage()
+      if (data) {
+        await storage.setAccessToken(data.authenticate.accessToken)
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-//   return (
-//     <Formik
-//       initialValues={initialValues}
-//       onSubmit={onSubmit}
-//       validationSchema={validationSchema}
-//     >
-//       {({ handleSubmit }) => (
-//         <View style={styles.formCard}>
-//           <FormikTextInput secure={false} name='username' placeholder='Username' />
-//           <FormikTextInput secure={true} name='password' placeholder='Password' />
-//           <Pressable style={styles.submitButton} onPress={handleSubmit}>
-//             <Text fontSize='regular' color='appBarText' fontWeight='bold'>Sign in</Text>
-//           </Pressable>
-//         </View>
-//       )}
-//     </Formik>
-//   );
-// };
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      {({ handleSubmit }) => (
+        <View style={styles.formCard}>
+          <FormikTextInput secure={false} name='username' placeholder='Username' />
+          <FormikTextInput secure={true} name='password' placeholder='Password' />
+          <Pressable style={styles.submitButton} onPress={() => handleSubmit}>
+            <Text fontSize='regular' color='appBarText' fontWeight='bold'>Sign in</Text>
+          </Pressable>
+        </View>
+      )}
+    </Formik>
+  );
+};
 
-// export default SignIn;
+export default SignIn;

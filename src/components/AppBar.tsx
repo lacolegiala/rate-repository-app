d36aa1f8ae5a4988a67f157f5724fb10b8e3558e
Sofia Riekkola <sofia.riekkola@gmail.com>
2024-06-27 14:38:19 +0300

    Remove unused import

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { View, StyleSheet, ScrollView, Pressable } from 'react-native';
 import Text from './Text';
 import Constants from 'expo-constants';

3fb898d2a9224e83b218942eb29b4bd037ebee35
Sofia Riekkola <sofia.riekkola@gmail.com>
2024-06-27 16:38:32 +0300

    Add a base for selector menu

--- src/components/RepositoryListContainer.tsx
+++ src/components/RepositoryListContainer.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlatList, View, StyleSheet, Pressable } from 'react-native';
 import { useNavigate } from 'react-router-native';
 import { Repository } from '../types';
 import RepositoryItem from './RepositoryItem';
+import { Button, Divider, Menu, PaperProvider } from 'react-native-paper';
 
 const styles = StyleSheet.create({
   separator: {
@@ -20,24 +21,44 @@ const RepositoryListContainer = ({ repositories }) => {
   const repositoryNodes = repositories
     ? repositories.edges.map(edge => edge.node)
     : [];
-    
-    const navigate = useNavigate()
 
-    const renderItem = (props: RenderItemProps) => {
+  const [visible, setVisible] = useState(false);
+
+  const openMenu = () => setVisible(true);
+
+  const closeMenu = () => setVisible(false);
     
-      return (
-        <Pressable onPress={() => navigate(`/${props.item.id}`)}>
-          <RepositoryItem item={props.item} />
-        </Pressable>
-      );
-    };
+  const navigate = useNavigate()
+
+  const renderItem = (props: RenderItemProps) => {
+  
+    return (
+      <Pressable onPress={() => navigate(`/${props.item.id}`)}>
+        <RepositoryItem item={props.item} />
+      </Pressable>
+    );
+  };
 
   return (
     <FlatList
       data={repositoryNodes}
       ItemSeparatorComponent={ItemSeparator}
       renderItem={renderItem}
-    />
+      ListHeaderComponent={
+      <PaperProvider>
+        <Menu
+          visible={visible}
+          onDismiss={closeMenu}
+          anchor={<Button onPress={openMenu}>Show menu</Button>}>
+          <Menu.Item onPress={() => {}} title="Item 1" />
+          <Menu.Item onPress={() => {}} title="Item 2" />
+          <Divider />
+          <Menu.Item onPress={() => {}} title="Item 3" />
+        </Menu>
+      </PaperProvider>
+      }
+    >
+    </FlatList>
   );
 };
 

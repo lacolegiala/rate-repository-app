66dae28ae60f079a96f282b0718f8f956b586197
Sofia Riekkola <sofia.riekkola@gmail.com>
2024-07-30 18:22:54 +0300

    Add searchbar

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,14 +3,21 @@ import useRepositories from "../hooks/useRepositories";
 import RepositoryListContainer from "./RepositoryListContainer";
 import { View } from "react-native";
 import { SortOptions } from "../types";
+import { Searchbar } from "react-native-paper";
 
 const RepositoryList = () => {
   const [sortOptions, setSortOptions] = useState<SortOptions>({orderBy: 'CREATED_AT', orderDirection: 'DESC'});
+  const [searchQuery, setSearchQuery] = React.useState('');
 
   const { repositories } = useRepositories({sortOptions: sortOptions});
 
   return (
     <View>
+      <Searchbar 
+        placeholder="Search"
+        onChangeText={setSearchQuery}
+        value={searchQuery}
+      />
       <RepositoryListContainer 
         repositories={repositories}
         sortOptions={sortOptions}
